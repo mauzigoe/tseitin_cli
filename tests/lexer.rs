c3f8a6fed7c85cb0bc100b081dfeef5630d49dff
Frangen, Leon <lesoftware@proton.me>
2024-12-23 23:02:27 +0100

    adapt tests; remove cnf checks due to rework;

--- tests/lexer.rs
+++ tests/lexer.rs
@@ -7,15 +7,15 @@ mod tests {
     #[test]
     fn test_lexer() {
 	let input = " aob_1 &(!Av1d | 1) | 0 \n".to_string();
-	let token_store = lex(input).unwrap();
+	let (token_store, _) = lex(input).unwrap();
 
 	let mut iter = token_store.iter();
 
-	assert_eq!(*iter.next().unwrap(), Token::Atom(Atom::Var("aob_1".to_string())));
+	assert_eq!(*iter.next().unwrap(), Token::Atom(Atom::Var(1)));
 	assert_eq!(*iter.next().unwrap(), Token::Op(Op::And));
 	assert_eq!(*iter.next().unwrap(), Token::LeftBracket);
 	assert_eq!(*iter.next().unwrap(), Token::Op(Op::Not));
-	assert_eq!(*iter.next().unwrap(), Token::Atom(Atom::Var("Av1d".to_string())));
+	assert_eq!(*iter.next().unwrap(), Token::Atom(Atom::Var(2)));
 	assert_eq!(*iter.next().unwrap(), Token::Op(Op::Or));
 	assert_eq!(*iter.next().unwrap(), Token::Atom(Atom::True));
 	assert_eq!(*iter.next().unwrap(), Token::RightBracket);
