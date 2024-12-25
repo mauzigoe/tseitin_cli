b18855f95e2afd8637b1266a53e0dfabb855f198
Frangen, Leon <lesoftware@proton.me>
2024-12-25 17:24:50 +0100

    update documentation for algorithm.rs

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,6 +1,6 @@
 use crate::{cnf::Cnf, var::VarStore, expr::{BiOp, Expr}, types::Atom};
 
-/// Perform tseitin encoding of [`Expr`]. Returns a [`CNF`](https://en.wikipedia.org/wiki/Conjunctive_normal_form)-conform [`Expr`]. 
+/// Perform tseitin encoding of [`Expr`]. Returns the [`CNF`](https://en.wikipedia.org/wiki/Conjunctive_normal_form)-conform tseitin transformation. 
 pub fn tseitin_encode(expr_input: &Expr, var_store: VarStore) -> Cnf {
     let mut cnf = Cnf::new(var_store);
     let c = tseitin_encode_inner(expr_input, &mut cnf);
@@ -9,8 +9,8 @@ pub fn tseitin_encode(expr_input: &Expr, var_store: VarStore) -> Cnf {
 }
 
 /// Inner implementation of [`tseitin_encode`].
-/// Takes [`Expr`] and incrementable [`var_count`] to track extra variables used for tseiting encoding.\\
-/// Returns [`Atom`], [`Option<Expr>`] . [`Atom`] is the identifier for the extra tseitin variable used to encode an boolean expression. [`Option<Expr>`] possibly contains the additional CNF expression introduced by the tseitin encoding, otherwise `None` (e.g. if [`tseitin_encode_inner`] is applied to [`Expr::Atom`]\([`Atom::Var`]\(x\)\))
+/// Takes [`Expr`] and [`Cnf`] to track extra variables used for tseiting encoding.\\
+/// Returns tseitin variable used for tseitin transformation. `tseitin_encode_inner` decends the [`Expr`]-tree in postorder fashion
 fn tseitin_encode_inner(expr: &Expr, cnf: &mut Cnf) -> i32 {
     match expr {
         Expr::BiOp(expr_left, op, expr_right) => {
@@ -32,7 +32,7 @@ fn tseitin_encode_inner(expr: &Expr, cnf: &mut Cnf) -> i32 {
     }
 }
 
-
+/// Adds boolean relation `c <==> a & b` to `Cnf` as CNF-conform formula. Here `c` is an additonal variable.
 pub fn and_cnf(cnf: &mut Cnf, a: i32, b: i32) -> i32 {
     let extra_lit = cnf.mut_var_store().new_extra_var() as i32;
     cnf.add_clause(vec![-extra_lit, a]);
@@ -41,6 +41,7 @@ pub fn and_cnf(cnf: &mut Cnf, a: i32, b: i32) -> i32 {
     extra_lit
 } 
 
+/// Adds boolean relation `c <==> a | b` to `Cnf` as CNF-conform formula. Here `c` is an additional variable.
 pub fn or_cnf(cnf: &mut Cnf, a: i32, b: i32) -> i32 {
     let extra_lit = cnf.mut_var_store().new_extra_var() as i32;
     cnf.add_clause(vec![extra_lit, -a]);
@@ -49,6 +50,7 @@ pub fn or_cnf(cnf: &mut Cnf, a: i32, b: i32) -> i32 {
     extra_lit
 } 
 
+/// Adds boolean relation `c <==> !a` to `Cnf` as CNF-conform formula. Here `c` is an additional variable. 
 pub fn not_cnf(cnf: &mut Cnf, a: i32) -> i32 {
     let extra_lit = cnf.mut_var_store().new_extra_var() as i32;
     cnf.add_clause(vec![-extra_lit, -a]);
@@ -56,12 +58,14 @@ pub fn not_cnf(cnf: &mut Cnf, a: i32) -> i32 {
     extra_lit
 }
 
+/// Adds boolean relation `c <==> true` to `Cnf` as CNF-conform formula. Here `c` is an additional variable.
 pub fn true_cnf(cnf: &mut Cnf) -> i32 {
     let extra_lit = cnf.mut_var_store().new_extra_var() as i32;
     cnf.add_clause(vec![extra_lit]);
     extra_lit
 }
 
+/// Adds boolean relation `c <==> false` to `Cnf` as CNF-conform formula. Here `c` is an additional variable.
 pub fn false_cnf(cnf: &mut Cnf) -> i32 {
     let extra_lit = cnf.mut_var_store().new_extra_var() as i32;
     cnf.add_clause(vec![-extra_lit]);
